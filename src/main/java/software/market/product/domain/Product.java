db59720626bde8d8177f0c38ec1fcbbedbab738c
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-07-03 17:16:15 +0900

    first commit

---
+++ src/main/java/software/market/product/domain/Product.java
@@ -0,0 +1,7 @@
+package software.market.product.domain;
+
+import jakarta.persistence.Entity;
+
+public class Product {
+
+}
---
+++ src/main/java/software/market/product/domain/ProductDocument.java
@@ -0,0 +1,5 @@
+package software.market.product.domain;
+
+public class ProductDocument {
+
+}
